53b8e2a85de9cf07b6b7d474d171689dba2a6481
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-03-05 11:34:17 +0300

    Add bean factory for auth-service

---
+++ auth-service/src/main/java/app/pomodorogo/auth/configuration/CustomBeanFactory.java
@@ -0,0 +1,14 @@
+package app.pomodorogo.auth.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class CustomBeanFactory {
+
+    @Bean
+    public BCryptPasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
