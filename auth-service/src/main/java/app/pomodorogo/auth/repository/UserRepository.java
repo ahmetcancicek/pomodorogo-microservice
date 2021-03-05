9c2c5cfb67324f0c027c28789c456b96f1b052d5
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-03-05 11:20:12 +0300

    Add repository for user

---
+++ auth-service/src/main/java/app/pomodorogo/auth/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package app.pomodorogo.auth.repository;
+
+import app.pomodorogo.auth.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User,String> {
+}
