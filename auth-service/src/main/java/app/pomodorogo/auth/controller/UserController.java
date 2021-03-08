2655133175f0ed46682b1ced07da84401a9ac9af
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-03-08 11:19:47 +0300

    Renamed controller

---
+++ auth-service/src/main/java/app/pomodorogo/auth/controller/UserController.java
@@ -0,0 +1,9 @@
+package app.pomodorogo.auth.controller;
+
+import app.pomodorogo.auth.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
