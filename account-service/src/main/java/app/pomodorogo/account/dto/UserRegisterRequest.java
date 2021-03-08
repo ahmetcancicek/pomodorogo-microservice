185097837dbb07c8e75f727ac342e00630ef9b18
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-03-08 13:39:31 +0300

    Add dto for user

---
+++ account-service/src/main/java/app/pomodorogo/account/dto/UserRegisterRequest.java
@@ -0,0 +1,31 @@
+package app.pomodorogo.account.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegisterRequest {
+
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    private String id;
+
+    @NotNull(message = "Username can not be null.")
+    @Size(min = 3,max = 50,message = "Username must be at least 3 characters")
+    private String username;
+
+    @NotNull
+    @Size(min = 10,max = 50)
+    private String email;
+
+    @NotNull
+    @Size(min = 4,max = 50)
+    private String password;
+}
