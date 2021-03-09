b9aa266e367163ee7aee970f61050bdbe9fc4134
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-03-09 11:49:17 +0300

    Update main class

--- auth-service/src/main/java/app/pomodorogo/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/app/pomodorogo/auth/AuthServiceApplication.java
@@ -2,10 +2,14 @@ package app.pomodorogo.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableResourceServer
+@EnableDiscoveryClient
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
