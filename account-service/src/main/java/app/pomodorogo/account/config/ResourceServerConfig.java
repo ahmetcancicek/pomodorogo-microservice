57e2a0b7340ffbab4a8ee82cb533940377c1b1be
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-03-12 12:08:45 +0300

    Done resource server config for account-service

--- account-service/src/main/java/app/pomodorogo/account/config/ResourceServerConfig.java
+++ account-service/src/main/java/app/pomodorogo/account/config/ResourceServerConfig.java
@@ -8,12 +8,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -43,21 +39,19 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     }
 
     @Bean
-    public OAuth2RestOperations restTemplate() {
+    public OAuth2RestTemplate clientCredentialsRestTemplate() {
         return new OAuth2RestTemplate(clientCredentialsResourceDetails());
     }
 
     @Bean
-    @Primary
-    public ResourceServerTokenServices resourceServerTokenServices() {
+    public ResourceServerTokenServices tokenServices() {
         return new UserInfoTokenServices(sso.getUserInfoUri(), sso.getClientId());
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/").permitAll()
-                .antMatchers(HttpMethod.OPTIONS, "/").permitAll()
+                .antMatchers("/").permitAll()
                 .anyRequest().authenticated();
     }
 }
