f50b8bbfaff8dea8449f1795648e77bbb5d7541f
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-03-12 12:07:48 +0300

    Fix for renamed method

--- auth-service/src/main/java/app/pomodorogo/auth/service/CustomUserDetailsService.java
+++ auth-service/src/main/java/app/pomodorogo/auth/service/CustomUserDetailsService.java
@@ -19,7 +19,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username)
+        return userRepository.findById(username)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
     }
 }
